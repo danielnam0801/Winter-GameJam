3d9524365f6866a0100203ef7050aa8d70bd39d7
leewnstj <ij35492581@gmail.com>
2023-01-12 00:22:48 +0900

    map 진행중

--- Winter-GameJam/Assets/01_Scripts/Spawn/SpawnManager.cs
+++ Winter-GameJam/Assets/01_Scripts/Spawn/SpawnManager.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    
+    public List<GameObject> Mabs = new List<GameObject>();
+
+    private void Start()
+    {
+        SpawnMap();
+    }
+
+    private void SpawnMap()
+    {
+        for(int i = 0; i < Mabs.Count; i++)
+        {
+            Instantiate(Mabs[i], new Vector2(i * 14.16f, -6.73F), Quaternion.identity);
+        }
+    }
 }
